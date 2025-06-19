c9a5da65997fe2d85dced1b8e4b6337b9bf46b89
Khin Su Wai <khinsu@xenoptics.com>
2025-06-19 12:19:05 +0700

    update next js project

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,10 @@
-import NextAuth from 'next-auth/next';
-import prisma from '@/lib/prismadb';
-import GoogleProvider from 'next-auth/providers/google';
-import { NextAuthOptions } from 'next-auth';
-import bcrypt from 'bcrypt';
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import CredentialsProvider from 'next-auth/providers/credentials';
+import NextAuth from "next-auth/next";
+import prisma from "@/lib/prismadb";
+import GoogleProvider from "next-auth/providers/google";
+import { NextAuthOptions } from "next-auth";
+import bcrypt from "bcrypt";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -13,21 +13,21 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
     CredentialsProvider({
-      id: 'credentials',
-      name: 'Credentials',
+      id: "credentials",
+      name: "Credentials",
       credentials: {
         email: {
-          label: 'Email',
-          type: 'text',
+          label: "Email",
+          type: "text",
         },
         password: {
-          label: 'Password',
-          type: 'passord',
+          label: "Password",
+          type: "passord",
         },
       },
       async authorize(credentials) {
         if (!credentials?.email || !credentials?.password) {
-          throw new Error('Email and password required');
+          throw new Error("Email and password required");
         }
 
         const user = await prisma.user.findUnique({
@@ -40,7 +40,7 @@ export const authOptions: NextAuthOptions = {
         });
 
         if (!user || !user.hashedPassword) {
-          throw new Error('Email does not exist');
+          throw new Error("Email does not exist");
         }
 
         const isCorrectPassword = await bcrypt.compare(
@@ -49,7 +49,7 @@ export const authOptions: NextAuthOptions = {
         );
 
         if (!isCorrectPassword) {
-          throw new Error('Incorrect password');
+          throw new Error("Incorrect password");
         }
 
         return user;
@@ -67,9 +67,9 @@ export const authOptions: NextAuthOptions = {
     },
   },
 
-  debug: process.env.NODE_ENV === 'development',
+  debug: process.env.NODE_ENV === "development",
   adapter: PrismaAdapter(prisma),
-  session: { strategy: 'jwt' },
+  session: { strategy: "jwt" },
 
   jwt: {
     secret: process.env.NEXTAUTH_JWT_SECRET,
@@ -78,5 +78,7 @@ export const authOptions: NextAuthOptions = {
 };
 
 const handler = NextAuth(authOptions);
+export const GET = handler;
+export const POST = handler;
 
-export { handler as GET, handler as POST };
+// export { handler as GET, handler as POST };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ['class'],
+  darkMode: ["class"],
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        Courgette: ['Courgette', 'sans-serif'],
+        Courgette: ["Courgette", "sans-serif"],
       },
       screens: {
-        xs: '480px',
-        ss: '620px',
-        sm: '768px',
-        md: '1060px',
-        lg: '1200px',
-        xl: '1700px',
+        xs: "480px",
+        ss: "620px",
+        sm: "768px",
+        md: "1060px",
+        lg: "1200px",
+        xl: "1700px",
       },
       colors: {
-        primaryDark: '#00040f',
-        secondaryDark: '#111315',
-        bgDark: '#1e2125',
-        bgBlue: '#0b7285',
-        bgBlue1: ' #3bc9db',
+        primaryDark: "#00040f",
+        secondaryDark: "#111315",
+        bgDark: "#1e2125",
+        bgBlue: "#0b7285",
+        bgBlue1: " #3bc9db",
       },
     },
   },
